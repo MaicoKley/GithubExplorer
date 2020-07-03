f6911a0c41bf8c49d84b2d023635cef6e1029759
Maico Alexandre Kley <maicokley@hotmail.com>
2020-07-02 21:22:39 -0300

    feat repository styles

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,11 @@
-import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
+
+import { Header, RepositoryInfo, Issues } from './styles';
+
+import logoImg from '../../assets/logo.svg';
 
 interface RepositoryParams {
   repository: string;
@@ -8,11 +14,60 @@ interface RepositoryParams {
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
 
+  useEffect(() => {
+    api.get(`repos/${params.repository}`).then(response => {
+      console.log(response.data);
+    });
+  }, [params.repository]);
+
   return (
-    <h1>
-      Repository:
-      {params.repository}
-    </h1>
+    <>
+      <Header>
+        <img src={logoImg} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars0.githubusercontent.com/u/27367456?v=4"
+            alt="Maico Kley"
+          />
+          <div>
+            <strong>maicokley/GoStack</strong>
+            <p>Blablablablbaba</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>10</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>3</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>0</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="sabonete">
+          <div>
+            <strong>Lalaalalla</strong>
+            <p>Wolololol</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
   );
 };
 
